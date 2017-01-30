17801af7caf9d0fe5731242d8d171fff661842be
ozzehbored <ozzeh@qq.com>
2017-01-30 05:33:39 -0600

    jk2 - overriding servername with servicename

--- lgsm/functions/info_config.sh
+++ lgsm/functions/info_config.sh
@@ -194,25 +194,27 @@ fn_info_config_factorio(){
 }
 
 # jo and ja
-#FIXME?: override servername with servicename? jk2/jk3 (and other quake games) can contain color codes (^#0-9 etc). alternatively, parse and display colors in console?
+# FIXME: produce regex which ignores set* commands and just reads the cvar value itself
+# CHANGEME: modify output to factor in quake color codes and display them properly?
 fn_info_config_jedi(){
 	if [ ! -f "${servercfgfullpath}" ]; then
 		rconpassword="${unavailable}"
-		servername="${unavailable}"
+#		servername="${unavailable}"
 		serverpassword="${unavailable}"
 		maxplayers="${zero}"
 	else
-		rconpassword=$(grep "rconpassword" "${servercfgfullpath}" | sed -e 's/^[ \t]*//g' -e '/^\//d' -e 's/rconpassword//g' | tr -d '=\";,:' | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
-		servername=$(grep "sv_hostname" "${servercfgfullpath}" | sed -e 's/^[ \t]*//g' -e '/^\//d' -e 's/sv_hostname//g' | tr -d '=\";,:' | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
-		serverpassword=$(grep "g_password" "${servercfgfullpath}" | sed -e 's/^[ \t]*//g' -e '/^\//d' -e 's/g_password//g' | tr -d '=\";,:' | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+		rconpassword=$(grep "rconpassword" "${servercfgfullpath}" | sed -e 's/^[ \t]*//g' -e '/^\//d' -e 's/set rconpassword//g' | tr -d '=\";,:' | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+#		servername=$(grep "sv_hostname" "${servercfgfullpath}" | sed -e 's/^[ \t]*//g' -e '/^\//d' -e 's/set sv_hostname//g' | tr -d '=\";,:' | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+		serverpassword=$(grep "g_password" "${servercfgfullpath}" | sed -e 's/^[ \t]*//g' -e '/^\//d' -e 's/set g_password//g' | tr -d '=\";,:' | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
 		maxplayers=$(grep "sv_maxclients" "${servercfgfullpath}" | grep -v "#" | tr -cd '[:digit:]')
 
 		# Not Set
 		rconpassword=${rconpassword:-"NOT SET"}
-		servername=${servername:-"NOT SET"}
+#		servername=${servername:-"NOT SET"}
 		serverpassword=${serverpassword:-"NOT SET"}
 		maxplayers=${maxplayers:-"0"}
 	fi
+	servername="${servicename}"
 }
 
 fn_info_config_minecraft(){
